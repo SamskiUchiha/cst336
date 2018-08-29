4b794cc16fb880512874806255df2453d664de0a
Sam Laitha <slaitha@csumb.edu>
2018-08-29 22:04:59 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
 
-echo "<h1> Sam Laitha </h1>";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Lab 1: Sam Laitha's Personal Website</title>
+    </head>
+    <!-- closing head -- >
 
+        <!-- This is the body -->
+        <!-- This is where we place the content of our website -->
+        <body>
+  
+            <header>
+                <h1> Sam Laitha</h1>
+            </header>
+            
+            <hr width = "50%"> 
+            
+            <nav>
+                
+                <a href = "index.html" > Home </a>
+                <a href = "about.html" > About </a>
+                <a href = "contact.html" > Contact Me </a>
+               
+                
+            </nav>
 
+              welcome to this <strong> world </strong>
+  
 
-
-
-?>
-</body>
+        </body>
 </html>
