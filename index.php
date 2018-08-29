4f6d8a827aac3b8486c5fc4d0fbb6e75193ab5fc
Sam Laitha <slaitha@csumb.edu>
2018-08-29 22:12:57 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
-        <title>Lab 1: Sam Laitha's Personal Website</title>
     </head>
     <!-- closing head -- >
 
@@ -16,16 +15,6 @@
                 <h1> Sam Laitha</h1>
             </header>
             
-            <hr width = "50%"> 
-            
-            <nav>
-                
-                <a href = "index.html" > Home </a>
-                <a href = "about.html" > About </a>
-                <a href = "contact.html" > Contact Me </a>
-               
-                
-            </nav>
   
         </body>
 </html>
